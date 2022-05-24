470fc987cb3efc4b77da72c3f8d1365d366eedcd
Riou <1982027616@qq.com>
2022-05-24 16:14:29 +0000

    !1 Merge branch 'master' of https://gitee.com/pymjl_0/payroll-management-system * Merge branch 'master' of https://gitee.com/pymjl_0/payroll-management-system * 创建前端项目 && 环境配置

---
+++ payroll/src/router/index.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: "/",
+      component: () => import("../views/HomePage.vue"),
+      children: [],
+    },
+  ],
+});
+
+export default router;
---
+++ payroll/src/views/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Vue3+axios+Naive</h1>
+  <n-button type="primary">start</n-button>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { NButton } from "naive-ui";
+
+export default defineComponent({
+  name: "HomePage",
+  components: { NButton },
+  props: {},
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
