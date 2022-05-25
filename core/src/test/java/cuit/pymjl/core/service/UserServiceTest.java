24fb85040eb928ce0bd8b849fefa7a563aed6f51
pymjl <1792243149@qq.com>
2022-05-25 17:11:11 +0800

    添加单元测试

---
+++ core/src/test/java/cuit/pymjl/core/service/UserServiceTest.java
@@ -0,0 +1,29 @@
+package cuit.pymjl.core.service;
+
+import cuit.pymjl.core.entity.user.dto.UserDTO;
+import cuit.pymjl.core.mapper.UserMapper;
+import cuit.pymjl.core.service.impl.UserServiceImpl;
+import cuit.pymjl.core.util.MybatisUtil;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Pymjl
+ * @version 1.0
+ * @date 2022/5/25 17:04
+ **/
+public class UserServiceTest {
+    UserService userService = new UserServiceImpl();
+
+    @Test
+    void register() {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setAccount("123435345435435341231");
+        userDTO.setUsername("123@qq.com");
+        userDTO.setAge(20);
+        userDTO.setGender(1);
+        userDTO.setNickname("pymjl");
+        userDTO.setPassword("123456");
+        userDTO.setPhone("19136310161");
+        System.out.println(userService.register(userDTO));
+    }
+}
