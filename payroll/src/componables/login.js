20dcce70bfb60c2674493aa8014cb96047bf1bbb
Riou <1982027616@qq.com>
2022-05-30 10:53:02 +0000

    !7 fix * fix * 修复请求错误提示信息无法显示问题 * 前端主页框架搭建 * 主页框架搭建 * 登录注册功能实现 * Merge remote-tracking branch 'origin/master' * Merge remote-tracking branch 'origin/master' * Merge branch 'master' of https://gitee.com/pymjl_0/payroll-management-system * 路由配置 && 登录、注册页面实现 * Merge remote-tracking branch 'origin/master' * Merge branch 'master' of https://gitee.com/pymjl_0/payroll-management-system * 创建前端项目 && 环境配置

--- payroll/src/componables/login.js
+++ payroll/src/componables/login.js
@@ -13,43 +13,63 @@ export default () => {
     code: "",
     emailCode: "",
   });
+
+  // 随机生成字符串
+  const randomString = () => {
+    const chars = "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678";
+    const maxPos = chars.length;
+    let str = "";
+    for (let i = 0; i < 6; i++) {
+      str += chars.charAt(Math.floor(Math.random() * maxPos));
+    }
+    return str;
+  };
+  const key = randomString();
   // 图片验证码
   const verifyCode = ref(null);
   const isLoading = ref(false);
   // 获取验证码
   const getCode = () => {
     isLoading.value = true;
-    if (!userInfo.value.username) {
-      message.error("请先输入账号");
-    } else {
-      axios({
-        method: "get",
-        url: "http://localhost:8080/code/image",
-        params: {
-          key: userInfo.value.username,
-        },
-        responseType: "blob",
-      }).then((res) => {
-        isLoading.value = false;
-        verifyCode.value = window.URL.createObjectURL(res.data);
-      });
-    }
+    axios({
+      method: "get",
+      url: "http://localhost:8080/code/image",
+      params: {
+        key: key,
+      },
+      responseType: "blob",
+    }).then((res) => {
+      isLoading.value = false;
+      verifyCode.value = window.URL.createObjectURL(res.data);
+    });
   };
+  getCode();
   // 获取邮箱验证码
   const getEmailCodeEvent = () => {
     const loading = message.loading("正在获取验证码...", { duration: 6000 });
-    getEmailCode(
-      userInfo.value.username,
-      userInfo.value.username,
-      userInfo.value.code
-    ).then(({ res, succeed }) => {
-      loading.destroy();
-      if (succeed) {
-        message.success("验证码已发送");
-      } else {
-        message.error(res.message);
-      }
-    });
+    getEmailCode(userInfo.value.username, key, userInfo.value.code)
+      .then(({ res, succeed }) => {
+        loading.destroy();
+        if (succeed) {
+          message.success("验证码已发送");
+        } else {
+          message.error(res.message);
+        }
+      })
+      .catch(
+        ({
+          response: {
+            data: { message: msg },
+          },
+        }) => {
+          // 更好理解的一种写法->解构err->message
+          // const {
+          //   data: { message: msg },
+          // } = err.response;
+          loading.destroy();
+          message.error(msg);
+        }
+      );
   };
   // 登录
   const signIn = () => {
@@ -58,16 +78,27 @@ export default () => {
       userInfo.value.username,
       userInfo.value.password,
       userInfo.value.emailCode
-    ).then(({ res, succeed }) => {
-      loading.destroy();
-      if (succeed) {
-        message.success("登录成功");
-        localStorage.setItem("token", res.data);
-        router.push("/");
-      } else {
-        message.error(res.message);
-      }
-    });
+    )
+      .then(({ res, succeed }) => {
+        loading.destroy();
+        if (succeed) {
+          message.success("登录成功");
+          localStorage.setItem("token", res.data);
+          router.push("/");
+        } else {
+          message.error(res.message);
+        }
+      })
+      .catch(
+        ({
+          response: {
+            data: { message: msg },
+          },
+        }) => {
+          loading.destroy();
+          message.error(msg);
+        }
+      );
   };
 
   const jump = () => {
--- payroll/src/componables/register.js
+++ payroll/src/componables/register.js
@@ -56,44 +56,61 @@ export default () => {
       message: "请填写电话号码",
     },
   };
+  // 随机生成字符串
+  const randomString = () => {
+    const chars = "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678";
+    const maxPos = chars.length;
+    let str = "";
+    for (let i = 0; i < 6; i++) {
+      str += chars.charAt(Math.floor(Math.random() * maxPos));
+    }
+    return str;
+  };
   // 图片验证码
   const verifyCode = ref(null);
   const isLoading = ref(false);
+  const key = randomString();
   // 获取验证码
   const getCode = () => {
     isLoading.value = true;
-    if (!model.value.username) {
-      message.error("请先输入账号");
-    } else {
-      axios({
-        method: "get",
-        url: "http://localhost:8080/code/image",
-        params: {
-          key: model.value.username,
-        },
-        responseType: "blob",
-      }).then((res) => {
+    axios({
+      method: "get",
+      url: "http://localhost:8080/code/image",
+      params: {
+        key: key,
+      },
+      responseType: "blob",
+    })
+      .then((res) => {
         isLoading.value = false;
         verifyCode.value = window.URL.createObjectURL(res.data);
-        console.log(verifyCode);
+      })
+      .catch((err) => {
+        const {
+          data: { message: msg },
+        } = err.response;
+        message.error(msg);
       });
-    }
   };
+  getCode();
   // 获取邮箱验证码
   const getEmailCodeEvent = () => {
-    const loading = message.loading("正在获取验证码...", { duration: 6000 });
-    getEmailCode(
-      model.value.username,
-      model.value.username,
-      model.value.code
-    ).then(({ res, succeed }) => {
-      loading.destroy();
-      if (succeed) {
-        message.success("验证码已发送");
-      } else {
-        message.error(res.message);
-      }
-    });
+    const loading = message.loading("正在获取验证码...", { duration: 4000 });
+    getEmailCode(model.value.username, key, model.value.code)
+      .then(({ res, succeed }) => {
+        loading.destroy();
+        if (succeed) {
+          message.success("验证码已发送");
+        } else {
+          message.error(res.message);
+        }
+      })
+      .catch((err) => {
+        const {
+          data: { message: msg },
+        } = err.response;
+        message.error(msg);
+      });
   };
   // 注册
   const registerEvent = () => {
@@ -107,15 +124,23 @@ export default () => {
       account: model.value.account,
       phone: model.value.phone,
       code: model.value.emailCode,
-    }).then(({ res, succeed }) => {
-      registering.destroy();
-      if (succeed) {
-        message.success("注册成功");
-        router.push("/login");
-      } else {
-        message.error(res.message);
-      }
-    });
+    })
+      .then(({ res, succeed }) => {
+        registering.destroy();
+        if (succeed) {
+          message.success("注册成功");
+          router.push("/login");
+        } else {
+          message.error(res.message);
+        }
+      })
+      .catch((err) => {
+        registering.destroy();
+        const {
+          data: { message: msg },
+        } = err.response;
+        message.error(msg);
+      });
   };
   return {
     router,
