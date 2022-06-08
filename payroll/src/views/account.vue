415d6bacb8d0e2924e82674d19c51ff2dff88782
Riou <1982027616@qq.com>
2022-06-08 16:52:18 +0000

    !27 1 * Merge branch 'master' of https://gitee.com/pymjl_0/payroll-management-system * test

--- payroll/src/views/account.vue
+++ payroll/src/views/account.vue
@@ -10,24 +10,51 @@
         <n-input :style="{ width: '50%' }" placeholder="根据ID查询" />
         <n-button type="primary"> 搜索 </n-button>
       </n-input-group>
-      <n-button type="info">工资明细</n-button>
+      <n-button type="info" @click="isShow">工资明细</n-button>
     </n-space>
   </n-card>
   <n-card style="margin-top: 10px">
     <n-h2 align="center">工资条</n-h2>
     <n-data-table
       :columns="columns"
-      :data="data"
+      :data="accountData"
       :pagination="pagination"
       :bordered="false"
       :loading="loading"
     />
   </n-card>
+  <!-- 模态框 -->
+  <n-modal v-model:show="showModal">
+    <n-card
+      style="width: 600px"
+      title="基本工资"
+      :bordered="false"
+      size="huge"
+      role="dialog"
+      aria-modal="true"
+    >
+      <n-form
+        ref="formRef"
+        :model="wage"
+        :rules="rules"
+        :size="normal"
+        label-placement="left"
+      >
+        <n-grid :cols="24" :x-gap="24">
+          <n-form-item-gi :span="12" label="普通员工" path="commonWage">
+            <n-input v-model:value="wage.commonWage" placeholder="Input" />
+          </n-form-item-gi>
+          <n-form-item-gi :span="12" label="部门经理" path="managerWage">
+            <n-input v-model:value="wage.managerWage" placeholder="Input" />
+          </n-form-item-gi>
+        </n-grid>
+      </n-form>
+    </n-card>
+  </n-modal>
 </template>
 
 <script>
-import { defineComponent, ref, onMounted, toRaw, h } from "vue";
-import { getBasicWage } from "../api/finance.js";
+import { defineComponent, h } from "vue";
 import {
   NCard,
   NInputGroup,
@@ -38,6 +65,10 @@ import {
   NDataTable,
   NTag,
   NH2,
+  NModal,
+  NForm,
+  NGrid,
+  NFormItemGi,
 } from "naive-ui";
 import useAccount from "../componables/account.js";
 
@@ -45,75 +76,65 @@ const statusInfo = [
   { title: "未发放", type: "error" },
   { title: "已发放", type: "success" },
 ];
-const createColumns = ({ sendMail }) => {
-  return [
-    {
-      title: "职工ID",
-      key: "employeeId",
+const columns = [
+  {
+    title: "职工ID",
+    key: "employeeId",
+    align: "center",
+  },
+  {
+    title: "职工姓名",
+    key: "name",
+    align: "center",
+  },
+  {
+    title: "基本工资",
+    key: "basicWage",
+    align: "center",
+  },
+  {
+    title: "奖金",
+    key: "bonus",
+    align: "center",
+  },
+  {
+    title: "罚款",
+    key: "fine",
+    align: "center",
+  },
+  {
+    title: "税收",
+    key: "tax",
+    align: "center",
+  },
+  {
+    title: "总工资",
+    key: "sumWage",
+    align: "center",
+  },
+  {
+    title: "银行卡号",
+    key: "accountNumber",
+    align: "center",
+  },
+  {
+    title: "状态",
+    key: "status",
+    align: "center",
+    render(row) {
+      return h(
+        NTag,
+        {
+          type: statusInfo[row.status].type,
+          secondary: true,
+        },
+        {
+          default: () => statusInfo[row.status].title,
+        }
+      );
     },
-    {
-      title: "职工姓名",
-      key: "name",
-    },
-    {
-      title: "基本工资",
-      key: "basicWage",
-    },
-    {
-      title: "奖金",
-      key: "bonus",
-    },
-    {
-      title: "罚款",
-      key: "fine",
-    },
-    {
-      title: "税收",
-      key: "tax",
-    },
-    {
-      title: "总工资",
-      key: "sumWage",
-    },
-    {
-      title: "银行卡号",
-      key: "accountNumber",
-    },
-    {
-      title: "状态",
-      key: "status",
-      render(row) {
-        return h(
-          NTag,
-          {
-            style: {
-              marginRight: "6px",
-            },
-            type: statusInfo[row.status].type,
-            secondary: true,
-          },
-          {
-            default: () => statusInfo[row.status].title,
-          }
-        );
-      },
-    },
-    {
-      title: "Action",
-      key: "actions",
-      render(row) {
-        return h(
-          NButton,
-          {
-            size: "small",
-            onClick: () => sendMail(row),
-          },
-          { default: () => "Send Email" }
-        );
-      },
-    },
-  ];
-};
+  },
+];
 
 export default defineComponent({
   name: "AccountVue",
@@ -126,40 +147,26 @@ export default defineComponent({
     NSpace,
     NDataTable,
     NH2,
+    NModal,
+    NForm,
+    NGrid,
+    NFormItemGi,
   },
   props: {},
   setup() {
-    const { accountData, loading, pagination } = useAccount();
-    const wage = ref(null);
-    onMounted(() => {
-      getBasicWage(1, 10)
-        .then(({ res }) => {
-          wage.value = toRaw(res.records);
-          console.log(wage.value);
-        })
-        .catch((err) => {
-          // 更好理解的一种写法->解构err->message
-          const {
-            data: { message: msg },
-          } = err.response;
-          console.log(msg);
-        });
-    });
+    const { accountData, loading, pagination, wage, showModal, isShow } =
+      useAccount();
     return {
       wage,
-      data: accountData,
-      columns: createColumns({
-        sendMail: () => {},
-      }),
+      accountData,
+      columns,
       loading,
       pagination,
+      showModal,
+      isShow,
     };
   },
 });
 </script>
 
-<style lang="scss" scoped>
-// .n-input-group {
-//   margin-left: 150px;
-// }
-</style>
+<style lang="scss" scoped></style>
